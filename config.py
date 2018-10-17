11231c215e55d824efa6570cecfc7a7da87f0f6b
yuma <fableooon@gmail.com>
2018-10-17 23:45:05 +0900

    no message

--- config.py
+++ config.py
@@ -54,8 +54,8 @@ grid        = EasyDict(size='1080p', layout='random')       # Options for train.
 #desc += '-celebahq';            dataset = EasyDict(tfrecord_dir='celebahq'); train.mirror_augment = True
 #desc += '-celeba';              dataset = EasyDict(tfrecord_dir='celeba'); train.mirror_augment = True
 #desc += '-cifar10';             dataset = EasyDict(tfrecord_dir='cifar10')
-#desc += '-shunga_mini';          dataset = EasyDict(tfrecord_dir='shunga_mini')
-desc += '-maico_flower';          dataset = EasyDict(tfrecord_dir='maico_flower')
+desc += '-shunga_mini';          dataset = EasyDict(tfrecord_dir='shunga_mini')
+#desc += '-maico_flower';          dataset = EasyDict(tfrecord_dir='maico_flower')
 #desc += '-cifar100';            dataset = EasyDict(tfrecord_dir='cifar100')
 #desc += '-svhn';                dataset = EasyDict(tfrecord_dir='svhn')
 #desc += '-mnist';               dataset = EasyDict(tfrecord_dir='mnist')
