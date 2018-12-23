ff804de2965a6dafcaf4a8ef43be7ac2bf0b2ff6
yuma <fableooon@gmail.com>
2018-12-23 16:16:14 +0900

    no message

--- networks.py
+++ networks.py
@@ -325,16 +325,15 @@ def D_paper(
             if lod > 0: x = cset(x, (lod_in < lod), lambda: grow(res + 1, lod - 1))
             x = block(x(), res); y = lambda: x
             if res > 2: y = cset(y, (lod_in > lod), lambda: lerp(x, fromrgb(downscale2d(images_in, 2**(lod+1)), res - 1), lod_in - lod))
-            print ('n', res, y().shape)
             return y()
         combo_out = grow(2, resolution_log2 - 2)
-        print ('~~')
+
         def grow_c(res, lod):
             x = lambda: fromrgb(downscale2d(images_in, 2**lod), res)
             if lod > 0: x = cset(x, (lod_in < lod), lambda: grow_c(res + 1, lod - 1))
             x = block(x(), res); y = lambda: x
             print ('in',res)
-            if res > 2: y = cset(y, (lod_in > lod), lambda: lerp(x, fromrgb(downscale2d(images_in, 2**(lod+1)), res - 1), lod_in - lod))
+            if res > 3: y = cset(y, (lod_in > lod), lambda: lerp(x, fromrgb(downscale2d(images_in, 2**(lod+1)), res - 1), lod_in - lod))
             print ('c', res, y().shape)
             return y()
         h4 = grow_c(2, resolution_log2 - 2)
