cb7989c94822ff0c39660756a261b33c6c7ced5d
yuma <fableooon@gmail.com>
2018-12-23 15:31:03 +0900

    no message

--- networks.py
+++ networks.py
@@ -321,6 +321,7 @@ def D_paper(
     if structure == 'recursive':
         def grow(res, lod):
             print (res)
+            print (x)
             x = lambda: fromrgb(downscale2d(images_in, 2**lod), res)
             if lod > 0: x = cset(x, (lod_in < lod), lambda: grow(res + 1, lod - 1))
             x = block(x(), res); y = lambda: x
