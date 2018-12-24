29afce3a0ea8270b377c634513497b6cf19418a5
yuma <fableooon@gmail.com>
2018-12-24 13:07:12 +0900

    no message

--- dataset_tool.py
+++ dataset_tool.py
@@ -641,8 +641,8 @@ def create_from_images_and_labels(tfrecord_dir, image_dir, label_dir, shuffle):
         error('Input images must be stored as RGB or grayscale')
 
     try:
-        with open(label_dir, 'r') as file:
-            labels = json.load(file)
+        with open(label_dir, 'rb') as file:
+            labels = pickle.load(file)
     except:
         error('Label file was not found')
 
@@ -663,8 +663,7 @@ def create_from_images_and_labels(tfrecord_dir, image_dir, label_dir, shuffle):
         for key in reordered_names:
             reordered_labels += [labels[key]]
             print ('label check :', key, labels[key])
-        reordered_labels = np.asarray(reordered_labels)
-        #reordered_labels = np.stack(reordered_labels, 0)
+        reordered_labels = np.stack(reordered_labels, 0)
         tfr.add_labels(reordered_labels)
 
 #----------------------------------------------------------------------------
