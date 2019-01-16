1389c56876f54a6c5ed6cf93c7ff4a0d8dd59dde
yuma <fableooon@gmail.com>
2019-01-16 16:07:05 +0900

    cond

--- train.py
+++ train.py
@@ -143,7 +143,7 @@ def train_progressive_gan(
     network_snapshot_ticks  = 1,           # How often to export network snapshots?
     save_tf_graph           = False,        # Include full TensorFlow computation graph in the tfevents file?
     save_weight_histograms  = False,        # Include weight histograms in the tfevents file?
-    resume_run_id           = 1,         # Run ID or network pkl to resume training from, None = start from scratch.
+    resume_run_id           = 001,         # Run ID or network pkl to resume training from, None = start from scratch.
     resume_snapshot         = 9620,         # Snapshot index to resume training from, None = autodetect.
     resume_kimg             = 9620,          # Assumed training progress at the beginning. Affects reporting and training schedule.
     resume_time             = 0.0):         # Assumed wallclock time at the beginning. Affects reporting.
